84de4579ce144678802b69c2e166af8680972354
abdelmalek <abdobenali887@gmail.com>
2023-11-04 21:15:25 +0100

    Python Script for Data Upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
+import datetime as dt
 
-
+time = dt.datetime.now()
+date_now = time.strftime("%d/%m/%Y")
+time_h_now = time.strftime("%I:%M:%S")
+sheet_app_api = "https://api.sheety.co/eef2f21b5cd52c5e74271578b4600a55/workoutTracking/workouts"
+new_row = "https://api.sheety.co/phill/myWebsite/emails"
 nutritionix_application_id = "ed341e25"
 nutritionix_apikey = "7726a49a2d43c7f62405079773ef5f7d"  # 45ccdff66bdbf9c9746ae9b3504658eb
 header_app = {
@@ -17,8 +22,23 @@ nutritionix_body = {
     "height_cm": 190.0,
     "age": 22
 }
+sheet_body = {
+    "Date": date_now,
+    "Time": time_h_now,
 
-response = requests.post(url=nutritionix_url, json=nutritionix_body, headers=header_app)
-print(response.text)
+}
+new_row_pi = {
+    "workout": {
+        "date": date_now,
+        "time": time_h_now,
+    }
+}
 
+# response = requests.post(url=nutritionix_url, json=nutritionix_body, headers=header_app)
+# data = response.json()["exercises"]
+# print(response.text)
 
+insert_data = requests.post(url=sheet_app_api, json=new_row_pi)
+print(insert_data)
+# workout_data = {i for i in data if i == "user_input"}
+# print(workout_data)
