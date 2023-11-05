f2c3480850c706f07405807ce6061f9c3c2318ea
abdelmalek <abdobenali887@gmail.com>
2023-11-05 21:21:56 +0100

    Google Sheets Exercise Tracker - Project Complete

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import requests
 import datetime as dt
+import os
 
+# Adding date and time
 time = dt.datetime.now()
 date_now = time.strftime("%d/%m/%Y")
-time_h_now = time.strftime("%I:%M:%S")
-sheet_app_api = "https://api.sheety.co/eef2f21b5cd52c5e74271578b4600a55/workoutTracking/workouts"
-new_row = "https://api.sheety.co/phill/myWebsite/emails"
-nutritionix_application_id = "ed341e25"
-nutritionix_apikey = "7726a49a2d43c7f62405079773ef5f7d"  # 45ccdff66bdbf9c9746ae9b3504658eb
+time_h_now = time.strftime("%X")
+
+# Sheet Auth
+MY_AUTH = os.environ["MY_AUTH"]
+sheet_app_api = os.environ['SHEET_APP_API']
+
+# Nutritionix APP ID and API Key. Actual values are stored as environment variables.
+nutritionix_application_id = os.environ['NUTRITIONIX_APPLICATION_ID']
+nutritionix_apikey = os.environ['NUTRITIONIX_APIKEY']
+
+nutritionix_url = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+# Nutritionix API Call
 header_app = {
     "x-app-key": nutritionix_apikey,
     "x-app-id": nutritionix_application_id
 }
 
-nutritionix_url = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_header = {
+    "Authorization": f"Basic {MY_AUTH}"
+}
 
 nutritionix_body = {
     "query": input("tell me which exercises you did: "),
@@ -27,18 +39,27 @@ sheet_body = {
     "Time": time_h_now,
 
 }
-new_row_pi = {
-    "workout": {
-        "date": date_now,
-        "time": time_h_now,
+
+response = requests.post(url=nutritionix_url, json=nutritionix_body, headers=header_app)
+data = response.json()["exercises"]
+workout_data = [{"name": item["name"], "duration_min": item["duration_min"], "nf_calories": item["nf_calories"]} for item in data]
+
+
+for exercise in workout_data:
+    new_row_pi = {
+        "workout": {
+            "date": date_now,
+            "time": time_h_now,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
     }
-}
+    send_to_sheet = requests.post(url=sheet_app_api, json=new_row_pi, headers=sheet_header)
 
-# response = requests.post(url=nutritionix_url, json=nutritionix_body, headers=header_app)
-# data = response.json()["exercises"]
-# print(response.text)
 
-insert_data = requests.post(url=sheet_app_api, json=new_row_pi)
-print(insert_data)
-# workout_data = {i for i in data if i == "user_input"}
-# print(workout_data)
+
+
+
+
+
